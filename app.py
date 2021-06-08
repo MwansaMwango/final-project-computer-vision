7772405b2685360d61f14f02f31c8a9a16837b37
Mwansa <Mwansad.mwango@gmail.com>
2021-06-09 01:13:45 +0800

    initial cvlib east detect using YOLOv4

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify, send_from_directory, send_file
-from simple_mmdetector.demo.detect import detectObj
-
+# from simple_mmdetector.demo.detect import detectObj
+from cvlib_count import easyDetect
 # Create an instance of our Flask app.
 # Static folder has files inside reachable for everyone.
 app = Flask(__name__)
@@ -19,12 +19,20 @@ def index():
 def demo():
     return render_template("demo.html")
 
-# Run detector 
-@app.route("/api/detect", methods=['GET'])
+# # Run detector 
+# @app.route("/api/detect", methods=['GET'])
+# def api_detect():
+#     results = detectObj()
+#     print(results)
+#     response = jsonify(results)
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     return response
+# Run easy detector 
+@app.route("/api/detect/person", methods=['GET'])
 def api_detect():
-    results = detectObj()
-    print(results)
-    response = jsonify(results)
+    output, label = easyDetect('static/image/frames/frame_0.jpg')
+    print(label)
+    response = jsonify(label)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -32,7 +40,7 @@ def api_detect():
 @app.route("/api/file-downloads", methods=['GET'])
 def api_file_downloads():
     try:
-        return send_from_directory(directory='./static/image/results', filename="result_frame_0.jpg", as_attachment=True, cache_timeout=0)
+        return send_from_directory(directory='./', filename="result_frame_0.jpg", as_attachment=True, cache_timeout=0)
     except Exception as e:
         return str(e)
 if __name__ == "__main__":
